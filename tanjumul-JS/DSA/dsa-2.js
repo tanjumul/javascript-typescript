3209e4d38c1dcc795b9537cd7ba20d0860a61ef8
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-14 00:00:56 +0600

    tracking : DSA-2

--- tanjumul-JS/DSA/dsa-2.js
+++ tanjumul-JS/DSA/dsa-2.js
@@ -192,6 +192,8 @@ if(isVowel){
 
 function checkVowel(char){ 
 
+  let check = char.toLowerCase();
+
 if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
   return 'vowel';
 }
