309488e3222e6f760d2e7bb05b7ca002346adc2c
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-17 15:59:39 +0600

    Something which is great to know on JS

---
+++ tanjumul-JS/arrow.js
@@ -0,0 +1,23 @@
+
+const user = { 
+  username:'Tanju', 
+  price : 999, 
+
+  welcomeMessage : function (){ 
+    console.log(`${this.username} Welcome to the TaliKhata App !`); 
+    console.log(this); 
+  }
+}
+/*
+user.welcomeMessage(); 
+user.username = 'Siam'; 
+user.welcomeMessage(); /** here reference just changed the values from tanjumul > siam */ 
+
+// console.log(this); 
+
+function koffe(){ 
+ let userName = 'siam'; 
+  console.log(this.userName); 
+}
+
+koffe(); 
--- tanjumul-JS/scopelevel.js
+++ tanjumul-JS/scopelevel.js
@@ -31,3 +31,21 @@ if(true){
   }
 }
 // console.log(username); 
+
+
+/**+++++++++++++++++++++++ Interessant +++++++++++++++++++++++++++++ */
+
+//you can declare a value before calling a function : 
+
+console.log(Calal(5)); 
+function Calal(num){ 
+  return num+1; 
+}
+
+/**see it will still work because of its declaration, it's a direct funciton if i would've stored it in a variable then the case would be different here ! */
+
+const powerTanju = function HeavyNum(n){ 
+  console.log(n+2); 
+ 
+}
+ console.log(powerTanju(6)); 
