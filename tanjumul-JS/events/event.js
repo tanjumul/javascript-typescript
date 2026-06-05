c181d737b9e84a708efa6e1ca79e5e1fc145d8c9
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-05 18:51:32 +0600

    removing with tagName

--- tanjumul-JS/events/event.js
+++ tanjumul-JS/events/event.js
@@ -31,7 +31,16 @@ document.getElementById('google').addEventListener('click', function(e){
 */ 
 
 document.querySelector('#images').addEventListener('click',function(e){ 
-console.log(e.target.parentNode); 
-let removeIt = e.target.parentNode; 
-removeIt.remove(); 
+// console.log(e.target.tagName); 
+if(e.target.tagName  === 'IMG'){ 
+  console.log(`Removed : ${e.target.id}`);
+  let removeIt = e.target.parentNode; 
+  removeIt.remove(); 
+}
+
+// let removeIt = e.target.parentNode; 
+// removeIt.remove(); 
+
+
+//removeIt.parentNode.removeChild(removeIt); another way to use this 
 }); 
