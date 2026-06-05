f75d76c1c8145827087728e8e8dd029620261d78
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-05 16:24:13 +0600

    target id works differently in event propagations

--- tanjumul-JS/events/event.js
+++ tanjumul-JS/events/event.js
@@ -1,7 +1,17 @@
-//capturing & bubbling (event) , e.stopPropagation() ,. e.defaultPrevent , e.preventDefault() 
+//event propagation : capturing & bubbling (event);  e.stopPropagation() ,. e.defaultPrevent , e.preventDefault() 
 
 
 
 /**
- * 
+ * in the event propagation no diff in true or false
  */
+
+
+document.getElementById('images').addEventListener('click',(e)=>{ 
+console.log('flase : clicked the ul picture'); 
+},false); 
+
+document.getElementById('owl').addEventListener('click',function(e){ 
+  console.log('true : Owl clicked'); 
+
+},true); 
