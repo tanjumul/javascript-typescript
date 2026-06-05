5bc5439578f9199fce7baaf017135b9839b86e18
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-05 18:40:05 +0600

    removing elements on clicks

--- tanjumul-JS/events/event.js
+++ tanjumul-JS/events/event.js
@@ -4,14 +4,34 @@
 
 /**
  * in the event propagation no diff in true or false
+ * ture : captureing propagation 
+ * false : bubbling from down to up
  */
-
-
+/*
+//capturing
 document.getElementById('images').addEventListener('click',(e)=>{ 
-console.log('flase : clicked the ul picture'); 
+console.log('clicked the ul picture'); 
+// e.stopPropagation(); 
 },false); 
 
-document.getElementById('owl').addEventListener('click',function(e){ 
-  console.log('true : Owl clicked'); 
 
-},true); 
+//bubbling 
+document.getElementById('owl').addEventListener('click',function(e){ 
+  console.log('Owl clicked'); 
+//e.stopPropagation(); //here it will never use the event bubbleing effect ! 
+},false); 
+
+
+document.getElementById('google').addEventListener('click', function(e){ 
+  e.preventDefault(); 
+  e.stopPropagation(); 
+  console.log('google clicked');
+},false); 
+
+*/ 
+
+document.querySelector('#images').addEventListener('click',function(e){ 
+console.log(e.target.parentNode); 
+let removeIt = e.target.parentNode; 
+removeIt.remove(); 
+}); 
