50cef727373ebf41373f98ed22381b17d1483f7c
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-15 23:30:22 +0600

    replace loop

--- tanjumul-JS/object_notations.js
+++ tanjumul-JS/object_notations.js
@@ -1,3 +1,16 @@
 let  obj = {
   name : 'Tanjumul',
 }
+
+function* numbers (){ 
+  let i = 0 ; 
+  while (true) yield i++; 
+}
+const val = numbers ()
+console.log(
+  .map(n => n * 2)
+  .take(5)
+  .toArray(), 
+);
+
+alert('Tanjumul using yield in replace of Loop')
