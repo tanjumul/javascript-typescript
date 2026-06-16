1d2294d7f047e7162c155c4e8d7daf908d247834
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-16 15:42:05 +0600

    alt loop

--- tanjumul-JS/object_loop_2.js
+++ tanjumul-JS/object_loop_2.js
@@ -1,1 +1,23 @@
-function new_loop
+function new_loop(point){
+for(let i = 0; i<= point; i++){
+  console.log(point)
+} 
+
+}
+
+new_loop(15);
+
+function* number(){ 
+  let i = 0; 
+  while(true) yield i++; 
+
+}
+
+const val = number(); 
+
+const result = []; 
+
+for(let i =0; i< 5; i++){ 
+  result.push(val.next().value);
+}
+console.log(result); 
