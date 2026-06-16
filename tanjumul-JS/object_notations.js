0253ca1066a144e1a4f01a90979bd4254bbbdd42
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-16 14:18:18 +0600

    calling up accumulator

--- tanjumul-JS/object_notations.js
+++ tanjumul-JS/object_notations.js
@@ -6,11 +6,12 @@ function* numbers (){
   let i = 0 ; 
   while (true) yield i++; 
 }
-const val = numbers ()
+const val = numbers();
 console.log(
-  .map(n => n * 2)
+
+  val.map(n => n * 2)
   .take(5)
   .toArray(), 
 );
 
-alert('Tanjumul using yield in replace of Loop')
+// console.log(alert('Tanjumul using yield in replace of Loop'));
