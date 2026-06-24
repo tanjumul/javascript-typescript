1b5dbcc56fb5a7dfa6addfabfc343f997a5d93c5
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-06-24 21:07:52 +0600

    promise

---
+++ Advanced/promise.js
@@ -0,0 +1,14 @@
+// promise has 3 states ; 
+/** 
+ * fullfiled 
+ * pending 
+ * rejected 
+ * 
+ * * 
+ */
+
+const promiseOne = new Promise(function(resolve , reject){ 
+  //do async tasks 
+  //Db task calls , crypto.
+  
+})
