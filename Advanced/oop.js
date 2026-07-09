5ce1425bee1fc4238f6cae43c9b4d4727864c886
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-07-09 23:50:59 +0600

    oop funds

---
+++ Advanced/oop.js
@@ -0,0 +1,12 @@
+/** pillars of OOPs
+ * 
+ * Abstraction - details will be hide , example : fetch
+ * 
+ * Encapsulation - wrapup all the mehtod and functions. Building a barriar which to access and which not to 
+ * 
+ * Inheritance : 
+ * 
+ * Polymorphism : console log is an example .
+ */
+
+
